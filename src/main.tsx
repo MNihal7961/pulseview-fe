14e5216c111d468371fcffb86e7bd2916daa6440
“MNihal7961 <mnihal7961@gmail.com>
2025-05-06 13:27:18 +0530

    feat: wrap App component with AuthContextProvider and LoaderProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { AuthContextProvider } from "./context/AuthContext.tsx";
+import "./index.css";
+import { LoaderProvider } from "./context/LoaderContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthContextProvider>
+      <LoaderProvider>
+        <App />
+      </LoaderProvider>
+    </AuthContextProvider>
+  </StrictMode>
+);
