9cbba5b113a44cc01176be6f68d5853b8d9f2785
“MNihal7961 <mnihal7961@gmail.com>
2025-05-06 22:04:32 +0530

    refactor: reorganize AuthService methods for improved readability

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,21 @@
+import type { SignInUserDTO, SignUpUserDTO } from "../types/dto";
 
-class AuthService{
+class AuthService {
+  async signUp(payload: SignUpUserDTO) {
+    try {
+      console.log(payload);
+    } catch (error: any) {
+      console.log(error);
+    }
+  }
 
+  async signIn(payload: SignInUserDTO) {
+    try {
+      console.log(payload);
+    } catch (error: any) {
+      console.log(error);
+    }
+  }
 }
 
 export const authService = new AuthService();
