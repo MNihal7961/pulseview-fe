0cd9628f224e824971114aed3794c12c6c1671d3
“MNihal7961 <mnihal7961@gmail.com>
2025-05-06 23:38:02 +0530

    refactor: enhance error handling and type definitions in AuthService methods

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,19 +1,46 @@
 import type { SignInUserDTO, SignUpUserDTO } from "../types/dto";
+import type { FailedAPIResponse, GetUserAPIResponse, SignInAPIResponse, SignUpAPIResponse } from "../types/types";
+import apiClient from "./api.client.service";
 
 class AuthService {
-  async signUp(payload: SignUpUserDTO) {
+  async signUp(
+    payload: SignUpUserDTO
+  ): Promise<SignUpAPIResponse | FailedAPIResponse> {
     try {
-      console.log(payload);
+      const response = await apiClient.post("/auth/signup", payload);
+      return response.data;
     } catch (error: any) {
-      console.log(error);
+      console.error("Error while signing up: ", error);
+      return {
+        success: false,
+        message: error.response.data.message,
+      };
     }
   }
 
-  async signIn(payload: SignInUserDTO) {
+  async signIn(payload: SignInUserDTO): Promise<SignInAPIResponse | FailedAPIResponse> {
     try {
-      console.log(payload);
+      const response = await apiClient.post("/auth/signin", payload);
+      return response.data;
     } catch (error: any) {
-      console.log(error);
+      console.error("Error while signing in:", error);
+      return {
+        success: false,
+        message: error.response.data.message,
+      };
+    }
+  }
+
+  async findUserById(id: string): Promise<GetUserAPIResponse | FailedAPIResponse> {
+    try{
+      const response = await apiClient.get(`/auth/users/${id}`);
+      return response.data;
+    }catch(error:any){
+      console.error("Error while finding user by id: ", error);
+      return {
+        success: false,
+        message: error.response.data.message,
+      };
     }
   }
 }
