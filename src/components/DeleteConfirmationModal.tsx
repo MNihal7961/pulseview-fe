aea573de197762250fd846f3298f7411edd1438f
“MNihal7961 <mnihal7961@gmail.com>
2025-05-12 23:10:51 +0530

    feat: implement DeleteConfirmationModal with confirmation buttons and description display

--- src/components/DeleteConfirmationModal.tsx
+++ src/components/DeleteConfirmationModal.tsx
@@ -1,3 +1,4 @@
+import { Modal } from "antd";
 import React from "react";
 
 interface DeleteConfirmationModalProps {
@@ -8,8 +9,41 @@ interface DeleteConfirmationModalProps {
   description: string;
 }
 
-const DeleteConfirmationModal: React.FC<DeleteConfirmationModalProps> = () => {
-  return <div>DeleteConfirmationModal</div>;
+const DeleteConfirmationModal: React.FC<DeleteConfirmationModalProps> = ({
+  description,
+  handleCancel,
+  handleConfirm,
+  handleClose,
+  isOpen,
+}) => {
+  return (
+    <Modal
+      maskClosable={false}
+      footer={null}
+      title={"Are you sure you ?"}
+      open={isOpen}
+      onCancel={handleClose}
+      centered
+    >
+      <div className="flex flex-col gap-4 w-full">
+        <p className="text-gray-500">{description}</p>
+        <div className="flex flex-row justify-end gap-4">
+          <button
+            onClick={handleCancel}
+            className="p-2 border border-[#003366] hover:border-[#002a48] text-[#003366] hover:text-[#002a48] font-normal rounded-[5px] !cursor-pointer"
+          >
+            Cancel
+          </button>
+          <button
+            onClick={handleConfirm}
+            className="p-2  bg-[#003366] text-white hover:bg-[#002a48] font-normal rounded-[5px] !cursor-pointer"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </Modal>
+  );
 };
 
 export default DeleteConfirmationModal;
