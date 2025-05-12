46eeeeb88debd55fec0127c578ae960adf6aa5ff
“MNihal7961 <mnihal7961@gmail.com>
2025-05-12 20:01:11 +0530

    feat: implement weight entries loading and display in WeightProgress component

--- src/pages/weight-progress/index.tsx
+++ src/pages/weight-progress/index.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { authContext } from "../../context/AuthContext";
+import { useLoader } from "../../context/LoaderContext";
+import type { WeightEntry } from "../../types/types";
+import { weightEntryService } from "../../services/weight.entry.service";
+import WeightEntriesTable from "../../components/WeightEntriesTable";
 
 const WeightProgress: React.FC = () => {
-  return <div>WeightProgress</div>;
+  const { user } = useContext(authContext);
+  const { setLoading, setLoadingMessage } = useLoader();
+  const [weightEntries, setWeightEntries] = useState<WeightEntry[]>([]);
+
+  useEffect(() => {
+    const init = async (userId: string) => {
+      try {
+        setLoading(true);
+        setLoadingMessage("Loading weight entries please wait...");
+        const response = await weightEntryService.findAllWeightEntry(userId);
+        if (response.success && response.data.length > 0) {
+          setWeightEntries(response.data);
+        } else {
+          setWeightEntries([]);
+        }
+      } catch (error: any) {
+        console.error("error while initializing: ", error);
+      } finally {
+        setLoading(false);
+        setLoadingMessage(null);
+      }
+    };
+    if (user && user._id) {
+      init(user._id);
+    }
+  }, [user]);
+
+  return (
+    <section className="mb-4 flex flex-col gap-y-4">
+      {/* <h1 className="text-xl text-[#002A48]">Upcoming Shipments</h1> */}
+      {/* <Carousel autoplay arrows dots>
+        {upComingShipments.map((item: ShipmentType, index) => (
+          <div key={index}>
+            <ShipmentCard key={index} shipment={item} />
+          </div>
+        ))}
+      </Carousel> */}
+      <h1 className="text-xl text-[#002A48]">All Weight Entries</h1>
+      <WeightEntriesTable data={weightEntries} />
+    </section>
+  );
 };
 
 export default WeightProgress;
