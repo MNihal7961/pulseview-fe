6b8d3ada5f45a61c33c59f317b5eeaf70bf42b6b
“MNihal7961 <mnihal7961@gmail.com>
2025-05-12 15:57:23 +0530

    style: change background color of add goal section to gray-50

--- src/components/GoalWidget.tsx
+++ src/components/GoalWidget.tsx
@@ -24,7 +24,7 @@ const GoalWidget: React.FC = () => {
   const renderAddGoalCTA = () => {
     const handleAddGoal = () => {};
     return (
-      <section className="bg-white">
+      <section className="bg-gray-50">
         <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
           <img
             className="w-full"
